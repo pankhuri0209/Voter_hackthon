ad403161fa4c99d2e3ba2dae619ad36b38d1195c
Pankhuri Gupta <gupta.pankh@northeastern.edu>
2024-02-18 05:05:49 -0500

    commit

--- voter/src/js/app.js
+++ voter/src/js/app.js
@@ -1,17 +1,25 @@
 // Get the button element by its ID
-var navigateButton = document.getElementById("quiz-page");
+// var navigateButton = document.getElementById("quiz-page");
 
-// Attach a click event listener to the button
-navigateButton.addEventListener("click", function () {
-  // Navigate to another HTML file
-  window.location.href = "quiz.html";
-});
+// // Attach a click event listener to the button
+// navigateButton.addEventListener("click", function () {
+//   // Navigate to another HTML file
+//   window.location.href = "quiz.html";
+// });
 
-function quizPage() {
-  var navigateButton = document.getElementById("quiz-page");
+// function quizPage() {
+//   var navigateButton = document.getElementById("quiz-page");
+//   // Attach a click event listener to the button
+//   navigateButton.addEventListener("click", function () {
+//     // Navigate to another HTML file
+//     window.location.href = "quiz.html";
+//   });
+// }
+function resultsPage() {
+  var navigateButton = document.getElementById("results-page");
   // Attach a click event listener to the button
   navigateButton.addEventListener("click", function () {
     // Navigate to another HTML file
-    window.location.href = "quiz.html";
+    window.location.href = "results.html";
   });
 }
